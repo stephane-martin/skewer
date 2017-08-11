7602c930c04a9624f4494342f93d12784f52071a
Stephane Martin <stephane.martin@vesperal.eu>
2017-08-11 18:44:43 +0200

    explicit json serialization

--- conf/conf.go
+++ conf/conf.go
@@ -183,34 +183,34 @@ func (l KafkaVersion) Greater(r KafkaVersion) bool {
 }
 
 type KafkaConfig struct {
-	Brokers                  []string      `mapstructure:"brokers" toml:"brokers"`
-	ClientID                 string        `mapstructure:"client_id" toml:"client_id"`
-	Version                  string        `mapstructure:"version" toml:"version"`
-	ChannelBufferSize        int           `mapstructure:"channel_buffer_size" toml:"channel_buffer_size"`
-	MaxOpenRequests          int           `mapstructure:"max_open_requests" toml:"max_open_requests"`
-	DialTimeout              time.Duration `mapstructure:"dial_timeout" toml:"dial_timeout"`
-	ReadTimeout              time.Duration `mapstructure:"read_timeout" toml:"read_timeout"`
-	WriteTimeout             time.Duration `mapstructure:"write_timeout" toml:"write_timeout"`
-	KeepAlive                time.Duration `mapstructure:"keepalive" toml:"keepalive"`
-	MetadataRetryMax         int           `mapstructure:"metadata_retry_max" toml:"metadata_retry_max"`
-	MetadataRetryBackoff     time.Duration `mapstructure:"metadata_retry_backoff" toml:"metadata_retry_backoff"`
-	MetadataRefreshFrequency time.Duration `mapstructure:"metadata_refresh_frequency" toml:"metadata_refresh_frequency"`
-	MessageBytesMax          int           `mapstructure:"message_bytes_max" toml:"message_bytes_max"`
-	RequiredAcks             int16         `mapstructure:"required_acks" toml:"required_acks"`
-	ProducerTimeout          time.Duration `mapstructure:"producer_timeout" toml:"producer_timeout"`
-	Compression              string        `mapstructure:"compression" toml:"compression"`
-	FlushBytes               int           `mapstructure:"flush_bytes" toml:"flush_bytes"`
-	FlushMessages            int           `mapstructure:"flush_messages" toml:"flush_messages"`
-	FlushFrequency           time.Duration `mapstructure:"flush_frequency" toml:"flush_frequency"`
-	FlushMessagesMax         int           `mapstructure:"flush_messages_max" toml:"flush_messages_max"`
-	RetrySendMax             int           `mapstructure:"retry_send_max" toml:"retry_send_max"`
-	RetrySendBackoff         time.Duration `mapstructure:"retry_send_backoff" toml:"retry_send_backoff"`
-	TLSEnabled               bool          `mapstructure:"tls_enabled" toml:"tls_enabled"`
-	CAFile                   string        `mapstructure:"ca_file" toml:"ca_file"`
-	CAPath                   string        `mapstructure:"ca_path" toml:"ca_path"`
-	KeyFile                  string        `mapstructure:"key_file" toml:"key_file"`
-	CertFile                 string        `mapstructure:"cert_file" toml:"cert_file"`
-	Insecure                 bool          `mapstructure:"insecure" toml:"insecure"`
+	Brokers                  []string      `mapstructure:"brokers" toml:"brokers" json:"brokers"`
+	ClientID                 string        `mapstructure:"client_id" toml:"client_id" json:"client_id"`
+	Version                  string        `mapstructure:"version" toml:"version" json:"version"`
+	ChannelBufferSize        int           `mapstructure:"channel_buffer_size" toml:"channel_buffer_size" json:"channel_buffer_size"`
+	MaxOpenRequests          int           `mapstructure:"max_open_requests" toml:"max_open_requests" json:"max_open_requests"`
+	DialTimeout              time.Duration `mapstructure:"dial_timeout" toml:"dial_timeout" json:"dial_timeout"`
+	ReadTimeout              time.Duration `mapstructure:"read_timeout" toml:"read_timeout" json:"read_timeout"`
+	WriteTimeout             time.Duration `mapstructure:"write_timeout" toml:"write_timeout" json:"write_timeout"`
+	KeepAlive                time.Duration `mapstructure:"keepalive" toml:"keepalive" json:"keepalive"`
+	MetadataRetryMax         int           `mapstructure:"metadata_retry_max" toml:"metadata_retry_max" json:"metadata_retry_max"`
+	MetadataRetryBackoff     time.Duration `mapstructure:"metadata_retry_backoff" toml:"metadata_retry_backoff" json:"metadata_retry_backoff"`
+	MetadataRefreshFrequency time.Duration `mapstructure:"metadata_refresh_frequency" toml:"metadata_refresh_frequency" json:"metadata_refresh_frequency"`
+	MessageBytesMax          int           `mapstructure:"message_bytes_max" toml:"message_bytes_max" json:"message_bytes_max"`
+	RequiredAcks             int16         `mapstructure:"required_acks" toml:"required_acks" json:"required_acks"`
+	ProducerTimeout          time.Duration `mapstructure:"producer_timeout" toml:"producer_timeout" json:"producer_timeout"`
+	Compression              string        `mapstructure:"compression" toml:"compression" json:"compression"`
+	FlushBytes               int           `mapstructure:"flush_bytes" toml:"flush_bytes" json:"flush_bytes"`
+	FlushMessages            int           `mapstructure:"flush_messages" toml:"flush_messages" json:"flush_messages"`
+	FlushFrequency           time.Duration `mapstructure:"flush_frequency" toml:"flush_frequency" json:"flush_frequency"`
+	FlushMessagesMax         int           `mapstructure:"flush_messages_max" toml:"flush_messages_max" json:"flush_messages_max"`
+	RetrySendMax             int           `mapstructure:"retry_send_max" toml:"retry_send_max" json:"retry_send_max"`
+	RetrySendBackoff         time.Duration `mapstructure:"retry_send_backoff" toml:"retry_send_backoff" json:"retry_send_backoff"`
+	TLSEnabled               bool          `mapstructure:"tls_enabled" toml:"tls_enabled" json:"tls_enabled"`
+	CAFile                   string        `mapstructure:"ca_file" toml:"ca_file" json:"ca_file"`
+	CAPath                   string        `mapstructure:"ca_path" toml:"ca_path" json:"ca_path"`
+	KeyFile                  string        `mapstructure:"key_file" toml:"key_file" json:"key_file"`
+	CertFile                 string        `mapstructure:"cert_file" toml:"cert_file" json:"cert_file"`
+	Insecure                 bool          `mapstructure:"insecure" toml:"insecure" json:"insecure"`
 }
 
 type JournaldConfig struct {
@@ -224,22 +224,22 @@ type JournaldConfig struct {
 }
 
 type AuditConfig struct {
-	Enabled         bool   `mapstructure:"enabled" toml:"enabled"`
-	SocketBuffer    int    `mapstructure:"socket_buffer" toml:"socket_buffer"`
-	EventsMin       int    `mapstructure:"events_min" toml:"events_min"`
-	EventsMax       int    `mapstructure:"events_max" toml:"events_max"`
-	MessageTracking bool   `mapstructure:"message_tracking" toml:"message_tracking"`
-	LogOutOfOrder   bool   `mapstructure:"log_out_of_order" toml:"log_out_of_order"`
-	MaxOutOfOrder   int    `mapstructure:"max_out_of_order" toml:"max_out_of_order"`
-	Appname         string `mapstructure:"appname" toml:"appname"`
-	Severity        int    `mapstructure:"severity" toml:"severity"`
-	Facility        int    `mapstructure:"facility" toml:"facility"`
-	TopicTmpl       string `mapstructure:"topic_tmpl" toml:"topic_tmpl"`
-	TopicFunc       string `mapstructure:"topic_function" toml:"topic_function"`
-	PartitionTmpl   string `mapstructure:"partition_key_tmpl" toml:"partition_key_tmpl"`
-	PartitionFunc   string `mapstructure:"partition_key_func" toml:"partition_key_func"`
-	FilterFunc      string `mapstructure:"filter_func" toml:"filter_func"`
-	ConfID          string `mapstructure:"-" toml:"-"`
+	Enabled         bool   `mapstructure:"enabled" toml:"enabled" json:"enabled"`
+	SocketBuffer    int    `mapstructure:"socket_buffer" toml:"socket_buffer" json:"socket_buffer"`
+	EventsMin       int    `mapstructure:"events_min" toml:"events_min" json:"events_min"`
+	EventsMax       int    `mapstructure:"events_max" toml:"events_max" json:"events_max"`
+	MessageTracking bool   `mapstructure:"message_tracking" toml:"message_tracking" json:"message_tracking"`
+	LogOutOfOrder   bool   `mapstructure:"log_out_of_order" toml:"log_out_of_order" json:"log_out_of_order"`
+	MaxOutOfOrder   int    `mapstructure:"max_out_of_order" toml:"max_out_of_order" json:"max_out_of_order"`
+	Appname         string `mapstructure:"appname" toml:"appname" json:"appname"`
+	Severity        int    `mapstructure:"severity" toml:"severity" json:"severity"`
+	Facility        int    `mapstructure:"facility" toml:"facility" json:"facility"`
+	TopicTmpl       string `mapstructure:"topic_tmpl" toml:"topic_tmpl" json:"topic_tmpl"`
+	TopicFunc       string `mapstructure:"topic_function" toml:"topic_function" json:"topic_function"`
+	PartitionTmpl   string `mapstructure:"partition_key_tmpl" toml:"partition_key_tmpl" json:"partition_key_tmpl"`
+	PartitionFunc   string `mapstructure:"partition_key_func" toml:"partition_key_func" json:"partition_key_func"`
+	FilterFunc      string `mapstructure:"filter_func" toml:"filter_func" json:"filter_func"`
+	ConfID          string `mapstructure:"-" toml:"-" json:"conf_id"`
 }
 
 type SyslogConfig struct {
