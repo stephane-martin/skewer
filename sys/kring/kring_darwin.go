f571973250a105fc8acb2cc0f10f46e28fed921d
Stephane Martin <stephane.martin@vesperal.eu>
2017-11-17 22:06:49 +0100

    key rings

---
+++ sys/kring/kring_darwin.go
@@ -0,0 +1,122 @@
+package kring
+
+import (
+	"crypto/rand"
+	"fmt"
+	"os"
+
+	"github.com/awnumar/memguard"
+	"github.com/keybase/go-keychain"
+	"golang.org/x/crypto/ed25519"
+)
+
+func storeSecret(service string, account string, label string, data *memguard.LockedBuffer) (err error) {
+	exec, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	item := keychain.NewItem()
+	item.SetSecClass(keychain.SecClassGenericPassword)
+	item.SetService(service)
+	item.SetAccount(account)
+	item.SetLabel(label)
+	item.SetData(data.Buffer())
+	item.SetAccess(&keychain.Access{
+		Label:               "skewer",
+		TrustedApplications: []string{exec},
+	})
+	return keychain.AddItem(item)
+}
+
+func getItem(service string, account string, label string) (res []byte, err error) {
+	query := keychain.NewItem()
+	query.SetSecClass(keychain.SecClassGenericPassword)
+	query.SetService(service)
+	query.SetAccount(account)
+	query.SetLabel(label)
+	query.SetMatchLimit(keychain.MatchLimitOne)
+	query.SetReturnData(true)
+	results, err := keychain.QueryItem(query)
+	if err != nil {
+		return nil, err
+	}
+	if len(results) > 1 {
+		return nil, fmt.Errorf("Too many results")
+	}
+	if len(results) == 1 {
+		return results[0].Data, nil
+	}
+	return nil, fmt.Errorf("Unknown secret")
+}
+
+func getSecret(service string, account string, label string) (secret *memguard.LockedBuffer, err error) {
+	item, err := getItem(service, account, label)
+	if err != nil {
+		return nil, err
+	}
+	secret, err = memguard.NewImmutableFromBytes(item)
+	if err != nil {
+		return nil, err
+	}
+	return secret, nil
+}
+
+func NewSignaturePubkey(session string) (pubkey *memguard.LockedBuffer, privkey *memguard.LockedBuffer, err error) {
+	pub, priv, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	pubkey, err = memguard.NewImmutableFromBytes(pub)
+	if err != nil {
+		return nil, nil, err
+	}
+	privkey, err = memguard.NewImmutableFromBytes(priv)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = storeSecret("skewer", session, "sigpubkey", pubkey)
+	if err != nil {
+		pubkey.Destroy()
+		privkey.Destroy()
+		return nil, nil, err
+	}
+	return pubkey, privkey, nil
+}
+
+func GetSignaturePubkey(session string) (pubkey *memguard.LockedBuffer, err error) {
+	pubkey, err = getSecret("skewer", session, "sigpubkey")
+	if err != nil {
+		return nil, err
+	}
+	return pubkey, nil
+}
+
+func NewBoxSecret(session string) (secret *memguard.LockedBuffer, err error) {
+	secretKey := make([]byte, 32)
+	_, err = rand.Read(secretKey)
+	if err != nil {
+		return nil, err
+	}
+	secret, err = memguard.NewImmutableFromBytes(secretKey)
+	if err != nil {
+		return nil, err
+	}
+	err = storeSecret("skewer", session, "boxsecret", secret)
+	if err != nil {
+		secret.Destroy()
+		return nil, err
+	}
+	return secret, nil
+}
+
+func GetBoxSecret(session string) (secret *memguard.LockedBuffer, err error) {
+	secret, err = getSecret("skewer", session, "boxsecret")
+	if err != nil {
+		return nil, err
+	}
+	return secret, nil
+}
+
+func DeleteBoxSecret(session string) error {
+	return keychain.DeleteGenericPasswordItem("skewer", session)
+}
---
+++ sys/kring/kring_linux.go
@@ -0,0 +1,94 @@
+package kring
+
+import (
+	"crypto/rand"
+	"fmt"
+
+	"github.com/awnumar/memguard"
+	"github.com/jsipprell/keyctl"
+	"golang.org/x/crypto/ed25519"
+)
+
+func getSecret(session string, label string) (pubkey *memguard.LockedBuffer, err error) {
+	keyring, err := keyctl.SessionKeyring()
+	if err != nil {
+		return nil, err
+	}
+	key, err := keyring.Search(fmt.Sprintf("skewer-%s-%s", label, session))
+	if err != nil {
+		return nil, err
+	}
+	data, err := key.Get()
+	if err != nil {
+		return nil, err
+	}
+	secret, err = memguard.NewImmutableFromBytes(data)
+	if err != nil {
+		return nil, err
+	}
+	return secret, nil
+}
+
+func GetSignaturePubkey(session string) (pubkey *memguard.LockedBuffer, err error) {
+	return getSecret(session, "sigpubkey")
+}
+
+func NewSignaturePubkey(session string) (pubkey *memguard.LockedBuffer, privkey *memguard.LockedBuffer, err error) {
+	pub, priv, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	pubkey, err = memguard.NewImmutableFromBytes(pub)
+	if err != nil {
+		return nil, nil, err
+	}
+	privkey, err = memguard.NewImmutableFromBytes(priv)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = storeSecret("skewer", session, "sigpubkey", pubkey)
+	if err != nil {
+		pubkey.Destroy()
+		privkey.Destroy()
+		return nil, nil, err
+	}
+	return pubkey, privkey, nil
+}
+
+func NewBoxSecret(session string) (secret *memguard.LockedBuffer, err error) {
+	secretKey := make([]byte, 32)
+	_, err = rand.Read(secretKey)
+	if err != nil {
+		return nil, err
+	}
+	keyring, err := keyctl.SessionKeyring()
+	if err != nil {
+		return nil, err
+	}
+	secret, err = memguard.NewImmutableFromBytes(secretKey)
+	if err != nil {
+		return nil, err
+	}
+	_, err = keyring.Add(fmt.Sprintf("skewer-boxsecret-%s", session), secret.Buffer())
+	if err != nil {
+		secret.Destroy()
+		return nil, err
+	}
+	return secret, nil
+}
+
+func GetBoxSecret(session string) (secret *memguard.LockedBuffer, err error) {
+	return getSecret(session, "boxsecret")
+}
+
+func DeleteBoxSecret(session string) error {
+	keyring, err := keyctl.SessionKeyring()
+	if err != nil {
+		return err
+	}
+	key, err := keyring.Search(fmt.Sprintf("skewer-boxsecret-%s", session))
+	if err != nil {
+		return err
+	}
+	return key.Unlink()
+}
