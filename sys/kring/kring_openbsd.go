3a727f04c7ce74ac61245d1965dfec1a9233d128
stef <stef@ruggy.vesperal.org>
2017-11-18 15:23:59 +0100

    update memguard for openbsd compat

---
+++ sys/kring/kring_openbsd.go
@@ -0,0 +1,32 @@
+package kring
+
+
+import (
+	"github.com/awnumar/memguard"
+)
+
+func GetSignaturePubkey(session string) (pubkey *memguard.LockedBuffer, err error) {
+	return nil, nil
+}
+
+
+func NewSignaturePubkey(session string) (pubkey *memguard.LockedBuffer, privkey *memguard.LockedBuffer, err error) {
+	return nil, nil, nil
+}
+
+
+func NewBoxSecret(session string) (secret *memguard.LockedBuffer, err error) {
+	return nil, nil
+}
+
+func GetBoxSecret(session string) (secret *memguard.LockedBuffer, err error) {
+	return nil, nil
+}
+
+func DeleteBoxSecret(session string) error {
+	return nil
+}
+
+func JoinSessionKeyRing() error {
+	return nil
+}
