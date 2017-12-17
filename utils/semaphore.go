4000b7e8a303a15a8d547490746d4754a375fb4b
Stephane Martin <stephane.martin@vesperal.eu>
2017-12-17 15:34:49 +0100

    semaphore using atomic

---
+++ utils/semaphore.go
@@ -0,0 +1,44 @@
+package utils
+
+import "sync/atomic"
+
+type Semaphore struct {
+	count    int32
+	disposed int32
+	spinlock int32
+}
+
+func NewSemaphore(total int32) *Semaphore {
+	return &Semaphore{count: total}
+}
+
+func (s *Semaphore) Acquire() error {
+	wait := ExpWait{}
+	// acquire spinlock
+	for !atomic.CompareAndSwapInt32(&s.spinlock, 0, 1) {
+		if atomic.LoadInt32(&s.disposed) == 1 {
+			return ErrDisposed
+		}
+		wait.Wait()
+	}
+	wait = ExpWait{}
+	atomic.AddInt32(&s.count, -1)
+	for atomic.LoadInt32(&s.count) < 0 {
+		if atomic.LoadInt32(&s.disposed) == 1 {
+			atomic.AddInt32(&s.count, 1)
+			atomic.StoreInt32(&s.spinlock, 0)
+			return ErrDisposed
+		}
+		wait.Wait()
+	}
+	atomic.StoreInt32(&s.spinlock, 0)
+	return nil
+}
+
+func (s *Semaphore) Release() {
+	atomic.AddInt32(&s.count, 1)
+}
+
+func (s *Semaphore) Dispose() {
+	atomic.StoreInt32(&s.disposed, 1)
+}
