c2b0eb7923eff6a56b1b4f380efc0e5baaf03a7c
Stephane Martin <stephane.martin@vesperal.eu>
2018-01-10 02:23:26 +0100

    update tail functions

--- utils/path.go
+++ utils/path.go
@@ -12,6 +12,13 @@ func IsDir(path string) bool {
 	return false
 }
 
+func IsFile(path string) bool {
+	if infos, err := os.Stat(path); err == nil {
+		return !infos.IsDir()
+	}
+	return false
+}
+
 func FileExists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil
