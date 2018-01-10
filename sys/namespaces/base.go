432ed615b27616925a13e8b6c14a0f07d835e65e
Stephane Martin <stephane.martin@vesperal.eu>
2018-01-10 02:24:41 +0100

    use fastwalk

--- sys/namespaces/base.go
+++ sys/namespaces/base.go
@@ -6,41 +6,47 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+
+	"github.com/mattn/go-zglob/fastwalk"
 )
 
-var hiddens []string = []string{
+var hiddens = []string{
 	"/lib/modules", "/lib/apparmor", "/lib/firmware", "/lib/modprobe.d", "/lib/recovery-mode", "/lib/udev", "/lib/brltty", "/lib/cgmanager",
 	"/lib/crda", "/lib/cryptsetup", "/lib/hdparm", "/lib/ifupdown", "/lib/init", "/lib/linux-sound-base", "/lib/ufw", "/lib/xtables",
 	"/lib/security",
 }
 
 func myWalk(root string) []string {
+	var mu sync.Mutex
+	sharedLibs := map[string]bool{}
 
-	shared_libs := map[string]bool{}
-
-	walk := func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
+	walk := func(path string, typ os.FileMode) error {
+		if typ.IsDir() {
 			if strings.Contains(path, "/security/") {
 				return filepath.SkipDir
 			}
-			if deriveAnyHidden(func(hidden string) bool { return strings.HasPrefix(path, hidden) }, hiddens) {
+			if deriveAnyHidden(func(hidden string) bool {
+				return strings.HasPrefix(path, hidden)
+			}, hiddens) {
 				return filepath.SkipDir
-			}
-			return nil
-		} else {
-			if strings.Contains(path, "/security/") {
-				return nil
-			}
-			if deriveAnyHidden(func(hidden string) bool { return strings.HasPrefix(path, hidden) }, hiddens) {
-				return nil
-			}
-			if strings.HasSuffix(path, ".so") || strings.Contains(path, ".so.") {
-				shared_libs[path] = true
 			}
 			return nil
 		}
+		if strings.Contains(path, "/security/") {
+			return nil
+		}
+		if deriveAnyHidden(func(hidden string) bool { return strings.HasPrefix(path, hidden) }, hiddens) {
+			return nil
+		}
+		if strings.HasSuffix(path, ".so") || strings.Contains(path, ".so.") {
+			mu.Lock()
+			sharedLibs[path] = true
+			mu.Unlock()
+		}
+		return nil
 	}
-	_ = filepath.Walk(root, walk)
-	return deriveKeys(shared_libs)
+	_ = fastwalk.FastWalk(root, walk)
+	return deriveKeys(sharedLibs)
 
 }
