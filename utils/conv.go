e09b9d86d60d16c7720fc6ec28e5f2810746d58f
Stephane Martin <stephane.martin@vesperal.eu>
2018-01-19 13:51:34 +0100

    sanity check

--- utils/conv.go
+++ utils/conv.go
@@ -1,12 +1,18 @@
 package utils
 
-import "strconv"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
 func Atoi32(s string) (int32, error) {
 	res, err := strconv.Atoi(s)
 	if err != nil {
 		return 0, nil
 	}
-	// TODO: check size ?
+	if res > math.MaxInt32 {
+		return 0, fmt.Errorf("int32 overflow")
+	}
 	return int32(res), nil
 }
