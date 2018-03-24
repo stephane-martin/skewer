618ce29535e1d0d1d38df80c83adae9e10028810
Stephane Martin <stephane.martin@vesperal.eu>
2018-03-24 22:49:46 +0100

    formatting output

--- cmd/testutmpx.go
+++ cmd/testutmpx.go
@@ -1,17 +1,3 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -27,28 +13,12 @@ import (
 var testutmpxCmd = &cobra.Command{
 	Use:   "testutmpx",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		b, _ := json.Marshal(utmpx.All())
+		b, _ := json.MarshalIndent(utmpx.All(), "", "  ")
 		fmt.Fprintln(os.Stderr, string(b))
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(testutmpxCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// testutmpxCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// testutmpxCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
