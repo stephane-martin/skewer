edba8f30018f0cf59ac71e6004592b95ed8c5963
Stephane Martin <stephane.martin@vesperal.eu>
2018-05-31 05:15:44 +0200

    time <-> bytes conversions

--- utils/conv.go
+++ utils/conv.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
+	"encoding/binary"
 	"fmt"
 	"math"
 	"strconv"
+	"time"
 )
 
 func Atoi32(s string) (int32, error) {
@@ -15,4 +17,22 @@ func Atoi32(s string) (int32, error) {
 		return 0, fmt.Errorf("int32 overflow")
 	}
 	return int32(res), nil
+}
+
+func Time2Bytes(t time.Time, dst []byte) []byte {
+	if cap(dst) < binary.MaxVarintLen32 {
+		dst = make([]byte, binary.MaxVarintLen32)
+	}
+	dst = dst[:binary.MaxVarintLen32]
+	n := binary.PutVarint(dst, t.UnixNano())
+	return dst[:n]
+}
+
+func Bytes2Time(b []byte) (time.Time, int) {
+	var ts time.Time
+	t, n := binary.Varint(b)
+	if n <= 0 {
+		return ts, n
+	}
+	return time.Unix(0, t), n
 }
